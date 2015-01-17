c733620c1d323f3c3e32b2169a7387525ff42cdb
KuroGuo <413298956@qq.com>
2015-01-17 15:58:09 +0800

    update

--- util/date.js
+++ util/date.js
@@ -1,8 +1,25 @@
 'use strict';
 
-exports.toDateString = function (date) {
-  var dateStr = date.getYear() + 1900 + '年';
-  dateStr += date.getMonth() + 1 + '月';
-  dateStr += date.getDate() + '日';
+exports.toDateString = function (date, separator) {
+  var dateStr = date.getYear() + 1900 + (separator || '年');
+  dateStr += date.getMonth() + 1 + (separator || '月');
+  dateStr += date.getDate() + (separator ? '' : '日');
   return dateStr;
+};
+
+exports.toDateTimeString = function (date, separator) {
+  var datetimeStr = this.toDateString(date, separator) + ' ';
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+
+  if (hours < 10)
+    hours = '0' + hours;
+
+  if (minutes < 10)
+    minutes = '0' + minutes;
+
+  datetimeStr += hours + ':';
+  datetimeStr += minutes;
+
+  return datetimeStr;
 };
